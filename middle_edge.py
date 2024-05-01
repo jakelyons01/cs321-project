486495e9ceb5be9a2ba8a4aa362d9a27edbe6891
jakelyons01 <jakelyons01@gmail.com>
2024-05-01 13:37:47 -0400

    made test file, started implementing windowed calculation. Going to lunch now

--- middle_edge.py
+++ middle_edge.py
@@ -10,6 +10,8 @@ from Bio.Align import substitution_matrices
 sub_mat = substitution_matrices.load("BLOSUM62")
 from enum import IntEnum
 
+INDEL = -5
+
 
 def read(filename):
     #read two amino acid strings from file
@@ -28,4 +30,25 @@ plan:
 
         return the (score? and) the start and end nodes for the edge
 """
+
+def middle_edge(seq1, seq2):
+    #finds middle edge in alignment graph
+    n = len(seq1)
+    m = len(seq2)
+    score = np.zeros((2, n+1), dtype=np.int_)
+    back = np.zeros((2, n), dtype=np.int_)
+
+    #initialize left edge of graph
+    for i in range(0, n):
+        score[0, i+1] = score[0, i] + INDEL
+
+    top_half = m/2 if m%2 == 0 else m//2 +1
+
+    for i in range(0, top_half):
+        #copy row1 to row0 if not first iteration
+        if i != 0:
+            np.copyto(score[0], score[1])
+            #NOTE: don't need to reset values of row1 to 0, we will simply overwrite them
+
+        #now run loop to calculate score in row1 over and over again
 
