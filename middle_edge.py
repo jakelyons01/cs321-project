ecfd1984a8dc5c75504a8c80d3596cd32f58ed5d
jakelyons01 <jakelyons01@gmail.com>
2024-05-01 13:03:55 -0400

    wrote plan

--- middle_edge.py
+++ middle_edge.py
@@ -7,6 +7,25 @@ Jake Lyons and Jaime Robinson
 import sys
 import numpy as np
 from Bio.Align import substitution_matrices
-sub_mat = substitution_matrices.load("PAM250")
+sub_mat = substitution_matrices.load("BLOSUM62")
 from enum import IntEnum
+
+
+def read(filename):
+    #read two amino acid strings from file
+    with open(filename, "r") as file: 
+       seq1 = list(file.readline().strip()) 
+       seq2 = list(file.readline().strip()) 
+
+    return seq1, seq2
+
+"""
+plan:
+    using sliding window:
+        compute score of immediate child
+        Save backtracking pointers
+        (then copy col2 info into col1 and repeat)
+
+        return the (score? and) the start and end nodes for the edge
+"""
 
