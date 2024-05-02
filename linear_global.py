a459d16e73343eda3e5496413119734965864e07
jakelyons01 <jakelyons01@gmail.com>
2024-05-02 15:53:47 -0400

    wrote plan

--- linear_global.py
+++ linear_global.py
@@ -19,6 +19,18 @@ class Back(IntEnum):
 IMPLEMENT FROM PSEUDO CODE ON vol1 p. 276
 """
 
+def middle_edge(top, bottom, left, right):
+    #finds middle edge and middle node given information
+    #call middle_edge.middle_edge(seq1, seq2)
+    #parse output to make it useful
+    return
+
 def linear_space_align(top, bottom, left, right):
     #recursively finds highest-scoring path in alignment graph in linear space
+    if left == right:
+        return #alignment formed by bottom - top vertical edges
+    if top == bottom:
+        return #alignment fromed by right - left horizontal edges
+
+    middle = (len(left) + len(right)) //2
     return
