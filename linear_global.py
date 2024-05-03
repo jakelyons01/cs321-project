f536da73d4e0c8a2f0c5b5928e3fe14392fe0c09
jakelyons01 <jakelyons01@gmail.com>
2024-05-02 22:09:57 -0400

    changed mid edge to print tuples instead of constructing from strings. Added some meat to the linear space alignment, still not done tho

--- linear_global.py
+++ linear_global.py
@@ -1,10 +1,10 @@
 """
-Linear Global Alignment 
+Linear Space Global Alignment 
 
 Jake Lyons and Jaime Robinson
 """
 
-import middle_edge
+from middle_edge import middle_edge
 import sys
 import numpy as np
 
@@ -19,11 +19,31 @@ class Back(IntEnum):
 IMPLEMENT FROM PSEUDO CODE ON vol1 p. 276
 """
 
-def middle_edge(top, bottom, left, right):
+def get_mid_edge(top, bottom, left, right):
     #finds middle edge and middle node given information
     #call middle_edge.middle_edge(seq1, seq2)
     #parse output to make it useful
-    return
+
+    back, score = middle_edge(seq1, seq2)
+    n = len(seq2)
+    top_half = int(n/2 if n%2 == 0 else n//2 +1)
+
+    score_trans = np.transpose(score)
+    longest = np.argmax(score_trans[1])
+    child = (longest, top_half)
+    back_pointer = back[longest-1][1]
+    parent=()
+
+    if back_pointer == Back.MAT:
+        parent = (longest -1, top_half -1) 
+
+    elif back_pointer == Back.VRT:
+        parent = (longest -1, top_half)
+
+    elif back_pointer == Back.HRZ:
+        parent = (longest, top_half-1) 
+
+    return parent, back_pointer
 
 def linear_space_align(top, bottom, left, right):
     #recursively finds highest-scoring path in alignment graph in linear space
@@ -33,4 +53,20 @@ def linear_space_align(top, bottom, left, right):
         return #alignment fromed by right - left horizontal edges
 
     middle = (len(left) + len(right)) //2
-    return
+    mid_node, mid_edge = get_mid_edge(a, b, c, d) 
+
+    #RECURSIVE CALL 1: top left box
+    path = linear_space_align(top, mid_node, left, middle)
+    path.append(mid_edge)
+    
+    if mid_edge == Back.HRZ or mid_edge == Back.MAT:
+        #middle <-- middle + 1
+
+    if mid_edge == Back.VRT or mid_edge == Back.MAT:
+        #mid_node <-- mid_node + 1
+
+    #RECURSIVE CALL 2: bottom right box
+    bot_right = linear_space_align(mid_node, bottom, middle, right)
+    path.append(bot_right)
+    
+    return path
