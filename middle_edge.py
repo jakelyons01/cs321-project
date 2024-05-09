ed588bbccf1e6091f56330213f78db5f351158ed
jakelyons01 <jakelyons01@gmail.com>
2024-05-09 15:33:43 -0400

    middle edge done

--- middle_edge.py
+++ middle_edge.py
@@ -26,22 +26,12 @@ def read(filename):
     return seq1, seq2
 
 
-# def copy_next_col(matrix):
-#     #copies col1 into col0 for given nx2 matrix
-#     trans = np.transpose(matrix)
-#     np.copyto(trans[0], trans[1])
-#     return np.transpose(trans)
-
 def middle_edge(seq1, seq2):
     #finds middle edge in alignment graph
     n = len(seq1)
     m = len(seq2)
     score = np.zeros((n+1, 2), dtype=np.int_)
     back = np.zeros((n+1, 2), dtype=np.int_)
-
-    #top_half = int(n/2 if n%2 == 0 else n//2 +1)
-    #top_half = m//2
-    #print(top_half)
 
     #initialize left edge of graph
     col = 0
@@ -50,17 +40,9 @@ def middle_edge(seq1, seq2):
         score[i, 0] = score[i-1, 0] + INDEL
         back[i, 0] = Back.VRT
         
-    #initialize top row
-    #score[0, 1] = score[0, 0] + INDEL
-
     #iterate window
     for j in range(1, m+1):
         col = j % 2 # Switch columns on each iteration
-        #copy row1 to row0
-#         if i != 1:
-#             score = copy_next_col(score)
-#             score[0,1] = score[0,0] + INDEL #account for indel
-#             back = copy_next_col(back)
 
         score[0, col] = score[0, col-1] + INDEL
         back[0, col] = Back.HRZ
@@ -76,14 +58,6 @@ def middle_edge(seq1, seq2):
             )
             score[i, col] = max(scores)
             back[i, col] = np.argmax(scores)
-#             if score[j, 1] == score[j-1, 1] + INDEL:
-#                 back[j-1, 0] = Back.VRT
-# 
-#             elif score[j, 1] == score[j, 0] + INDEL:
-#                 back[j-1, 0] = Back.HRZ
-# 
-#             elif score[j, 1] == score[j-1, 0] + sub_mat[(seq1[j-1], seq2[i-1])]:
-#                 back[j-1, 0] = Back.MAT
 
     return back[:,col], score[:,col]
 
@@ -111,22 +85,3 @@ if __name__ == "__main__":
     
     print(start, end)
     
-#     top_half = int(n//2 +1)
-#     
-#     fs_score = np.transpose(fs_score)
-#     ts_score = np.transpose(ts_score)
-#     longest = np.argmax(fs_score[1] + ts_score[0][::-1])
-#     child = (longest, top_half)
-#     back_ptr = back[longest-1][1]
-#     parent=()
-#     
-#     if back_ptr == Back.MAT:
-#         parent = (longest -1, top_half -1) 
-# 
-#     elif back_ptr == Back.VRT:
-#         parent = ( longest -1, top_half)
-# 
-#     elif back_ptr == Back.HRZ:
-#         parent = (longest, top_half-1) 
-# 
-#     print(parent, child)
